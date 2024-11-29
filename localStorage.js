17ec1a71b1c5f647b22c49c7ad823d99acee1711
ckg2427 <ckg2427@naver.com>
2024-11-29 09:51:19 +0900

    로그인 회원가입 파일을 별도로 구성

---
+++ localStorage.js
@@ -0,0 +1,31 @@
+// setItem(key, value): 데이터를 저장합니다.
+
+// javascript
+// 코드 복사
+// localStorage.setItem('username', 'Alice');
+// getItem(key): 저장된 데이터를 가져옵니다.
+
+// javascript
+// 코드 복사
+// const username = localStorage.getItem('username');
+// console.log(username); // "Alice"
+// removeItem(key): 특정 데이터를 삭제합니다.
+
+// javascript
+// 코드 복사
+// localStorage.removeItem('username');
+// clear(): 모든 데이터를 삭제합니다.
+
+// javascript
+// 코드 복사
+// localStorage.clear();
+// length: 저장된 항목의 개수를 반환합니다.
+
+// javascript
+// 코드 복사
+// console.log(localStorage.length); // 저장된 키 개수
+// key(index): 지정된 인덱스의 키를 반환합니다.
+
+// javascript
+// 코드 복사
+// console.log(localStorage.key(0)); // 첫 번째 키
